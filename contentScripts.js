4edcdc621d0c3999bbacb4fe024bd085f326d8b4
xmavv <xxmatheooo@gmail.com>
2023-09-26 21:15:20 +0200

    link gets color that user wants to from input

--- contentScripts.js
+++ contentScripts.js
@@ -1,47 +1,85 @@
-// import setColor from './popup.js'
-
 const link = document.querySelectorAll('a');
 const apiKey = 'AIzaSyC4_ToUySpg0UxJ0gzkNm6pFpHKBjAz2OE';
 const apiUrl = `https://safebrowsing.googleapis.com/v4/threatMatches:find?key=${apiKey}`;
 
-link.forEach(e => {
-    let link_href = e.getAttribute('href')
-
-const requestData =   {
-    client: {
-      clientId:      "ID",
-      clientVersion: "1.0.0"
-    },
-    threatInfo: {
-      threatTypes:      ["MALWARE", "SOCIAL_ENGINEERING"],
-      platformTypes:    ["WINDOWS"],
-      threatEntryTypes: ["URL"],
-      threatEntries: [
-        {url: `${link_href}`}
-      ]
-    }
-  }
-
-  fetch(apiUrl, {
-    method: 'POST',
-    body: JSON.stringify(requestData)
-  })
-    .then(response => response.json()) // .json() a nie JSON.parse(), bo .json() wykorzystwyany jest z fetch
-    .then(data => {
-      console.log(data);
-      if (data.matches && data.matches.length > 0) e.style.color = "red"
-    })
-    .catch(error => {
-      console.error('Wystąpił błąd:', error);
-    })
-})
-
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if(request.greeting === "hello")
-    sendResponse({farewell: "goodbye"})
+          console.log(request.message);
   }
-)
+);
+
+
+
+
+// const witam = (request, sender, sendResponse) => {
+//   if( request.message === "start" ) {
+//       console.log(request.message)
+//       console.log(request.fun)
+//       return request.fun
+//   }
+// }
+
+function api (color) {
+  link.forEach(e => {
+    let link_href = e.getAttribute('href')
+
+    const requestData = {
+      client: {
+        clientId:      "ID",
+        clientVersion: "1.0.0"
+      },
+      threatInfo: {
+        threatTypes:      ["MALWARE", "SOCIAL_ENGINEERING"],
+        platformTypes:    ["WINDOWS"],
+        threatEntryTypes: ["URL"],
+        threatEntries: [
+          {url: `${link_href}`}
+        ]
+      }
+    }
+
+    fetch(apiUrl, {
+      method: 'POST',
+      body: JSON.stringify(requestData)
+    })
+      .then(response => response.json()) // .json() a nie JSON.parse(), bo .json() wykorzystwyany jest z fetch
+      .then(data => {
+        chrome.runtime.onMessage.addListener(
+          function(request, sender, sendResponse) {
+                  console.log(request.message);
+                  if (data.matches && data.matches.length > 0) e.style.color = request.message;
+                }
+          );
+      })
+      .catch(error => {
+        console.error('Wystąpił błąd:', error);
+      })
+  })
+}
+
+api();
+
+
+async function conncet () {
+  // const getColorz = await getColor();
+  // await api(getColor);
+  // console.log(getColorz);
+}
+
+conncet();
+
+
+
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     console.log('dziala1')
+//     if(request.greeting === "hello"){
+//       alert('siema')
+//       console.log('dziala2')
+//     }
+//   }
+// )
 
 
 // for now it only check links that are staticly added by developer throught html
--- popup.js
+++ popup.js
@@ -1,17 +1,34 @@
 const unSafeColorPicker = document.getElementById("input_unsafe");
 const unSafeLink = document.querySelector(".unsafe");
+let color = 'red';
 
 function setColor (input, link) {
 
     input.addEventListener("input", () => {
         link.style.cssText = `color: ${input.value};`;
+        color = input.value;
     })
-
-    return input.value
 }
 
 setColor(unSafeColorPicker, unSafeLink);
 
-chrome.runtime.sendMessage({greeting: "hello", function(response){
-    console.log(response.farewell)
-}})
+
+
+function popup() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        const activeTab = tabs[0];
+        console.log(siema);
+        chrome.tabs.sendMessage(activeTab.id, {message: color});
+    });
+}
+
+document.getElementById("input_unsafe").addEventListener("input", popup);
+
+
+
+
+
+
+// chrome.runtime.sendMessage({greeting: "hello", function(response){
+//     console.log(response.farewell)
+// }})
