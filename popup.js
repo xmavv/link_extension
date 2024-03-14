2084e7ec18f745c1e5db16e73c629a3cbf7571fc
xmavv <xxmatheooo@gmail.com>
2024-03-14 23:16:40 +0100

    really mess here but at least app is working now, next task to clean up this all code

--- popup.js
+++ popup.js
@@ -1,24 +1,32 @@
 const unSafeColorPicker = document.getElementById("input_unsafe");
+const SafeColorPicker = document.getElementById("input_safe");
 const unSafeLink = document.querySelector(".unsafe");
-let color = "#000";
+let unSafeColor = unSafeColorPicker.value;
+let SafeColor = SafeColorPicker.value;
 
 function setColor(input, link) {
   input.addEventListener("input", () => {
     link.style.cssText = `color: ${input.value};`;
-    color = input.value;
+    unSafeColor = input.value;
   });
 }
 
 setColor(unSafeColorPicker, unSafeLink);
 
+//TO JEST TU MEGA zle zrobione XD ten kod to jakis zart
+
 function popup() {
   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     const activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, { message: color });
+    chrome.tabs.sendMessage(activeTab.id, { message: unSafeColor });
   });
 }
 
-document.getElementById("input_unsafe").addEventListener("input", popup);
+popup();
+
+document.document;
+unSafeColorPicker.addEventListener("input", popup);
+SafeColorPicker.addEventListener("input", popup);
 
 function debounce(callback, delay = 10000) {}
 
