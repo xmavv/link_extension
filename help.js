a6f7d656fec26cf59d3095d92453b6a19a9313e0
xmavv <xxmatheooo@gmail.com>
2024-03-26 02:47:30 +0100

    testing Observer instance

--- help.js
+++ help.js
@@ -1,10 +1,9 @@
 const button = document.getElementById("button");
-const container = document.getElementById("div1");
 
 button.addEventListener("click", () => {
   const link = document.createElement("a");
   link.href = "https://testsafebrowsing.appspot.com/s/malware.html";
   link.innerHTML = "CLICK ME";
 
-  container.appendChild(link);
+  document.body.appendChild(link);
 });
